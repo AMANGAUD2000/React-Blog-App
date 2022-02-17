c77965a2d02407e7236aa504c7e8fed21d9e680a
AMANGAUD2000 <aman.gaud2000@gmail.com>
2022-02-17 10:30:34 +0530

    min Task required completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,12 +10,28 @@ function Home() {
     const getPosts = async () => {
       const data = await getDocs(postsCollectionRef);
 
-      console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   });
 
-  return <div className="homePage"> Home </div>;
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.author.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
